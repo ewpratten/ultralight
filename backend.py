702e60951bb9cad359933bbdd0ba7b22ed424eba
Evan Pratten <ewpratten@gmail.com>
2020-09-10 11:03:52 -0400

    Rewrite the backend

--- backend.py
+++ backend.py
@@ -4,147 +4,32 @@ import json
 import yaml
 from datetime import datetime
 import os
-import random
-import hashlib
+from webapputils import Webapp
 
 # Flask app configuration
-app = flask.Flask(__name__, static_folder="static")
-app.config['JSON_SORT_KEYS'] = False
+app = Webapp(__name__, static_directory="static", google_tracking_code=os.environ.get("GA_TRACKING_ID", ""))
 
 # Handle Github tokens
 GH_ID = os.environ.get("GH_CLI_ID", "")
 GH_PRIV = os.environ.get("GH_CLI_PRIV", "")
-GA_TRACKING_ID = os.environ.get("GA_TRACKING_ID", "")
 
 # Caching
 CACHE_SECONDS = 60
-
-# Google analytics namespace
-
-def ga_generateRandomUID() -> str:
-    """Generate a random UID string for a tracking identifier
-
-    Returns:
-        str: Random tracking UID
-    """
-    return hashlib.md5(str(random.random()).encode()).hexdigest()
-
-
-def ga_trackPath(url, uid=ga_generateRandomUID()):
-    data = {
-        'v': '1',  # API Version.
-        'tid': GA_TRACKING_ID,  # Tracking ID / Property ID.
-        # Anonymous Client Identifier. Ideally, this should be a UUID that
-        # is associated with particular user, device, or browser instance.
-        'cid': uid,
-        't': "pageview",
-        'dp': url,
-        'ua': 'Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14'
-    }
-
-    # Call the collection system
-    ga_mkGACollectionRequest(data)
-    
-    
-def ga_trackEvent(category, action, uid=ga_generateRandomUID()):
-    data = {
-        'v': '1',  # API Version.
-        'tid': GA_TRACKING_ID,  # Tracking ID / Property ID.
-        # Anonymous Client Identifier. Ideally, this should be a UUID that
-        # is associated with particular user, device, or browser instance.
-        'cid': uid,
-        't': 'event',  # Event hit type.
-        'ec': category,  # Event category.
-        'ea': action,  # Event action.
-        'el': None,  # Event label.
-        'ev': 0,  # Event value, must be an integer
-        'ua': 'Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14'
-    }
-
-    # Call the collection system
-    ga_mkGACollectionRequest(data)
-
-
-
-def ga_mkGACollectionRequest(data: dict):
-    try:
-        requests.post(
-            'https://www.google-analytics.com/collect', data=data)
-    except requests.exceptions.ConnectionError as e:
-        print("Failed to make tracking request")
-
-
-def trackAPICall(url, uid=ga_generateRandomUID()):
-
-    # Make a log entry
-    print(f"A request has been made to {url} with a UID of {uid}")
-
-    # Call event tracker
-    ga_trackEvent(
-        "APICall",
-        url,
-        uid
-    )
-
-    # Call path tracker
-    ga_trackPath(
-        url,
-        uid
-    )
-
-def trackError(url, error, uid=ga_generateRandomUID()):
-    trackAPICall(f"{url}?internal_error={error}", uid=uid)
-
-def getBrowserFingerprint() -> str:
-    return hashlib.md5(flask.request.headers.get('User-Agent').encode()).hexdigest()
-
-@app.errorhandler(404)
-def error404(e):
-
-    # Track this event with GA
-    trackAPICall(
-        "/404",
-        uid=getBrowserFingerprint()
-    )
-
-    return flask.jsonify({
-        "success": False,
-        "message": "not found",
-        "error":str(e)
-    }), 404
-
-@app.errorhandler(500)
-def error500(e):
-
-    # Track this event with GA
-    trackError(
-        "/error",
-        "500"
-    )
-
-    return flask.jsonify({
-        "success": False,
-        "message":"an application error ocurred",
-        "error":str(e)
-    }), 500
-
 
 # Function to load the sources yml
 def loadSourcesYML() -> dict:
     return yaml.load(open("sources.yml"))
 
 # Index route
-@app.route("/")
+@app.flask.route("/")
 def handleIndex():
-
-    # Track request
-    trackAPICall(
-        "/",
-        uid=getBrowserFingerprint()
-    )
+    app.trackPageFetch("/")
 
     # Load the index
     index = open("static/index.html", "r").read()
+
+    # Push the analytics tracking ID
+    index = index.replace("{GA_TRACKING_ID}", app._google_tracking_code)
 
     # Build a flask response
     res = flask.make_response(index)
@@ -155,14 +40,9 @@ def handleIndex():
     return res
 
 # Sources route
-@app.route("/sources.yml")
+@app.flask.route("/sources.yml")
 def handleSources():
-
-    # Track request
-    trackAPICall(
-        "/sources.yml",
-        uid=getBrowserFingerprint()
-    )
+    app.trackPageFetch("/sources.yml")
 
     # Load the sources file
     sources = open("sources.yml", "r").read()
@@ -179,14 +59,9 @@ def handleSources():
     return res
 
 # Sources api
-@app.route("/api/sources")
+@app.flask.route("/api/sources")
 def handleSourcesAPI():
-
-    # Track request
-    trackAPICall(
-        "/api/sources",
-        uid=getBrowserFingerprint()
-    )
+    app.trackAPICall("/api/sources")
 
     # Get the YML config
     sources = loadSourcesYML()["sources"]
@@ -212,14 +87,9 @@ def handleSourcesAPI():
 
     return res
 
-@app.route("/api/artifact/<group>/<artifact>/versions")
+@app.flask.route("/api/artifact/<group>/<artifact>/versions")
 def handleArtifactAPI(group, artifact):
-
-    # Track request
-    trackAPICall(
-        f"/api/artifact/{group}/{artifact}/versions",
-        uid=getBrowserFingerprint()
-    )
+    app.trackAPICall(f"/api/artifact/{group}/{artifact}/versions")
 
     # Check that the artifact exists
     repocode = ""
@@ -258,14 +128,9 @@ def handleArtifactAPI(group, artifact):
 
     return res
 
-@app.route("/api/artifact/<group>/<artifact>/shield")
+@app.flask.route("/api/artifact/<group>/<artifact>/shield")
 def handleArtifactShieldAPI(group, artifact):
-
-    # Track request
-    trackAPICall(
-        f"/api/artifact/{group}/{artifact}/shield",
-        uid=getBrowserFingerprint()
-    )
+    app.trackAPICall(f"/api/artifact/{group}/{artifact}/shield")
 
     # Check that the artifact exists
     repocode = ""
@@ -377,91 +242,113 @@ def fetchJAR(url, fmt):
 
     return flask.make_response("Not found", 404)
 
-# Maven handler
-@app.route("/maven/<path:path>")
-def handleMaven(path):
-
-    # Track request
-    trackAPICall(
-        f"/api/{path}",
-        uid=getBrowserFingerprint()
-    )
-
-    # Parse filepath
-    pathComponents = path.split("/")
-
-    # Get the requested filename
-    filename = pathComponents[-1]
-    file_ext = filename.split(".")[-1]
-
-    # Handle non-metadata requests
-    version: str
-    artifact: str
-    group: str
-    len4 = False
-    if len(pathComponents) > 4:
-        len4 = True
-    
-        # Get the version
-        version = pathComponents[-2]
-
-        # Get the artifactID
-        artifact = pathComponents[-3]
-
-        # Get the groupID
-        group = ".".join(pathComponents[:-3])
-    if len(pathComponents) == 4 or filename == "maven-metadata.xml":
-        # Get the artifactID
-        artifact = pathComponents[-2]
-
-        # Get the groupID
-        group = ".".join(pathComponents[:-2])
-    elif not len4:
-        return "Artifact not found (parse error)", 404
-
-    # Check that the artifact exists
-    repocode = ""
-    fmt = ""
+# Fetches github data from YML file
+def getGitHubArtifactData(group, artifact) -> dict:
     for source in loadSourcesYML()["sources"]:
         if source["groupID"] == group and source["artifactID"] == artifact:
-            repocode = source["github"]["owner"] + "/" + source["github"]["repository"]
-            fmt = source["github"]["assetFormat"]
-            break
+            return {
+                "repository": source["github"]["owner"] + "/" + source["github"]["repository"],
+                "asset_pattern": source["github"]["assetFormat"]
+            }
     else:
+        return None
+
+# metadata handler
+@app.flask.route("/maven/<path:group>/<artifact>/maven-metadata.xml")
+def handleMetadata(group: str, artifact: str):
+    """Handles generating a metadata file
+
+    Args:
+        group (str): Raw group id string
+        artifact (str): Raw artifactID string
+
+    Returns:
+        flask.response: generated metadata
+    """
+    app.trackAPICall(f"/maven/{group}/{artifact}/maven-metadata.xml")
+
+    # Convert group to a groupID
+    groupID = ".".join(group.split("/"))
+
+    # Get data about where to find the artifact
+    artifact_data = getGitHubArtifactData(groupID, artifact)
+
+    # Handle the artifact not being available
+    if artifact_data == None:
         return "Artifact not on this server", 404
 
-    # Get a list of valid versions
-    allAssetVersions = getAllValidVersions(repocode)
+    # Get a list of valid versions for the asset
+    all_versions = getAllValidVersions(artifact_data["repository"])
+    
+    # Build and return a response
+    res = flask.make_response(generateMavenMetadata(groupID, artifact, all_versions))
+    res.headers.set("content-type", "application/xml")
+    res.headers.set('Cache-Control', f"s-maxage={CACHE_SECONDS}, stale-while-revalidate")
+    return res
 
-    # Handle requests to maven-metadata
-    if filename == "maven-metadata.xml":
-        res = flask.make_response(generateMavenMetadata(group, artifact, allAssetVersions))
-        res.headers.set("content-type", "application/xml")
-        res.headers.set('Cache-Control', f"s-maxage={CACHE_SECONDS}, stale-while-revalidate")
-        return res
+# POM handler
+@app.flask.route("/maven/<path:group>/<artifact>/<version>/<file>.pom")
+def handlePOM(group: str, artifact: str, version: str, file: str):
+    """Generate a POM file
 
-    # Make sure the requested version exists
-    if version not in allAssetVersions.keys():
-        return "Artifact does not have this version (yet?)", 404
+    Args:
+        group (str): Artifact group
+        artifact (str): Artifact
+        version (str): Version
+        file (str): Filename
 
-    # If the request is for a pom file, generate one
-    if file_ext == "pom":
-        res = flask.make_response(generatePOMForPackage(group, artifact, version))
-        res.headers.set("content-type", "application/java-archive")
-        res.headers.set('Cache-Control', f"s-maxage={CACHE_SECONDS}, stale-while-revalidate")
-        return res
+    Returns:
+        flask.response: Generated POM
+    """
 
-    if file_ext == "jar":
+    app.trackAPICall(f"/maven/{group}/{artifact}/{version}/{file}.pom")
 
-        # Get the asset URL for this version
-        assetURL = allAssetVersions[version]["url"]
+    # Convert group to a groupID
+    groupID = ".".join(group.split("/"))
 
-        res = fetchJAR(assetURL, fmt.replace("{version}", version))
-        res.headers.set('Cache-Control', f"s-maxage={CACHE_SECONDS}, stale-while-revalidate")
-        return res
+    # Build and return response
+    res = flask.make_response(generatePOMForPackage(groupID, artifact, version))
+    res.headers.set("content-type", "application/java-archive")
+    res.headers.set('Cache-Control', f"s-maxage={CACHE_SECONDS}, stale-while-revalidate")
+    return res
 
-    return "Artifact not found", 404
+@app.flask.route("/maven/<path:group>/<artifact>/<version>/<file>.jar")
+def handleJAR(group: str, artifact: str, version: str, file: str):
+    """Get the requested JAR file
+
+    Args:
+        group (str): Artifact group
+        artifact (str): Artifact
+        version (str): Version
+        file (str): Filename
+
+    Returns:
+        flask.response: JAR file
+    """
+
+    app.trackAPICall(f"/maven/{group}/{artifact}/{version}/{file}.jar")
+
+    # Convert group to a groupID
+    groupID = ".".join(group.split("/"))
+
+    # Get data about where to find the artifact
+    artifact_data = getGitHubArtifactData(groupID, artifact)
+
+    # Handle the artifact not being available
+    if artifact_data == None:
+        return "Artifact not on this server", 404
+
+    # Get a list of valid versions for the asset
+    all_versions = getAllValidVersions(artifact_data["repository"])
+
+    # Determine the URL to the actual asset
+    asset_url = all_versions[version]["url"]
+
+    # Build and return a response
+    res = fetchJAR(asset_url, artifact_data["asset_pattern"].replace("{version}", version))
+    res.headers.set('Cache-Control', f"s-maxage={CACHE_SECONDS}, stale-while-revalidate")
+    return res
 
 # Runner for local development
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.flask.run(debug=True)
